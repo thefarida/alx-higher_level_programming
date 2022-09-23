ac1cbee15408c3aab6962181581c6a31a0cac7c7
thefarida <adamuibrahimfarida@gmail.com>
2022-09-23 19:17:07 +0100

    Handles basic operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,6 +4,15 @@ from calculator_1 import add, sub, mul, div
 
 if __name__ == '__main__':
     
+    """
+    Handles basic operations
+
+    Performs basic operations like addition, substraction,
+    multiplication and division between two numbers.
+
+    The program will execute an operation between two numbers
+    selected by the operator sent to the program.
+    """
 
     l_av = len(sys.argv) - 1
     
