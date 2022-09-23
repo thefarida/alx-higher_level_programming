6b35f6ce19435a262f0ec7956a9c7c4d629d8ed8
thefarida <adamuibrahimfarida@gmail.com>
2022-09-23 15:35:06 +0100

    Addition of all arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,12 +2,12 @@
 import sys
 
 if __name__ == "__main__":
-    ag = sys.argv
-    l_ag = len(ag)
+    av = sys.argv
+    l_av = len(av)
     sum = 0
 
-if l_ag > 1:
-    for i in range(1, l_ag):
-        sum += int(ag[i])
+if l_av > 1:
+    for i in range(1, l_av):
+        sum += int(av[i])
 
     print(sum)
