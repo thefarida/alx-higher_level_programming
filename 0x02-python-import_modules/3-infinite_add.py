b9df301c35b9d910bd70cb0bcf7915363f6c5534
thefarida <adamuibrahimfarida@gmail.com>
2022-09-23 14:48:27 +0100

    Addition of all arguments

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    sum = 0
+
+if len(sys.argv) > 1:
+    for i in range(1, len(sys.argv)):
+        sum += int(sys.argv[i])
+
+    print(sum)
