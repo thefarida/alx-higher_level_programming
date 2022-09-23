20241feeb86e6bc06e731a789a2c424751d547e7
thefarida <adamuibrahimfarida@gmail.com>
2022-09-23 15:44:21 +0100

    Addition of all arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     l_av = len(av)
     sum = 0
 
-if l_av > 1:
-    for i in range(1, l_av):
-        sum += int(av[i])
+    if l_av > 1:
+        for i in range(1, l_av):
+            sum += int(av[i])
 
     print(sum)
