68639a546d8d54c41551bc537f83f0ebd83f038f
thefarida <adamuibrahimfarida@gmail.com>
2022-09-23 13:58:02 +0100

    Multiple Operations

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
 
+"""Prints the addition, subtraction, multiplication and division of 10 and 5"""
+
 if __name__ == "__main__":
 
 a = 10
