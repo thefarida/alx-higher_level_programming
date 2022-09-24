51cc0030dfe28588f378006894bea31a2c77341f
thefarida <adamuibrahimfarida@gmail.com>
2022-09-24 16:17:58 +0100

    Addition of tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,6 +5,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
     return ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
 
+
 def validate_tuple(_tuple=()):
     if len(_tuple) < 2:
         if len(_tuple) == 1:
