d1e653edc0fcece8cb932969b2a91bb2389b8a94
thefarida <adamuibrahimfarida@gmail.com>
2022-09-29 00:35:22 +0100

    Convert Roman figures to Integers

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,12 +3,12 @@ def roman_to_int(roman_string):
     if type(roman_string) is not str or roman_string is None:
         return 0
 
-    roman_dict = [["I", 1], ["V", 5], ["X", 10], ["L", 50],["C", 100], ["D", 500], ["M", 1000]]
+    roman_letters = [["I", 1], ["V", 5], ["X", 10], ["L", 50],["C", 100], ["D", 500], ["M", 1000]]
     num = 0
     last = 0
 
     for letter in roman_string:
-        for elem in roman_dict:
+        for elem in roman_letters:
             if letter == elem[0]:
                 if last == 0 or last >= elem[1]:
                     num += elem[1]
