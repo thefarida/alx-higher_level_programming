81716230a2c9e0e991b049053a95367eb5318cfd
thefarida <adamuibrahimfarida@gmail.com>
2022-09-28 18:58:56 +0100

    Squares of matrices

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,7 +3,7 @@ def square_matrix_simple(matrix=[]):
     new_matrix = []
 
     if len(matrix) > 0
-    for i in range(len(matrix)):
-        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
+    for elems in matrix[:]:
+        new_matrix.append(list(map(lambda x: x**2, matrix[i])))
 
     return new_matrix
