367ad88bb8ae592c0631faba8ed0475513273b03
thefarida <adamuibrahimfarida@gmail.com>
2022-09-29 01:38:38 +0100

    Convert roman figures to integrs

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,10 @@ def roman_to_int(roman_string):
     if type(roman_string) is not str or roman_string is None:
         return 0
 
-    roman_dict = [['I', 1], ['V', 5], ['X', 10], ['L', 50], ['C', 100], ['D', 500], ['M', 1000]]
+    roman_dict = [
+            ['I', 1], ['V', 5], ['X', 10], ['L', 50],
+            ['C', 100], ['D', 500], ['M', 1000]
+            ]
     num = 0
     last = 0
 
