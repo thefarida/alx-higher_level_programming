f8875aededee23430cce6aab70c6d4c390124317
thefarida <adamuibrahimfarida@gmail.com>
2022-10-10 17:40:51 +0100

    Print x elments of a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,12 +5,12 @@ def safe_print_list(my_list=[], x=0):
     idx = 0
 
     try:
-            for i in my_list:
-                if idx < x:
-                    print('{}'.format(my_list[idx]), end='')
-                    idx += 1
+        for i in my_list:
+            if idx < x:
+                print('{}'.format(my_list[idx]), end='')
+                idx += 1
 
-            print()        
+        print()
     except (IndexError, TypeError, ValueError):
         pass
     finally:
