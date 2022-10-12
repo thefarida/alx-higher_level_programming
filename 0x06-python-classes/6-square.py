59757eaf83544304664e78b76e4157c77702d841
thefarida <adamuibrahimfarida@gmail.com>
2022-10-12 14:18:22 +0100

    Update the position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -30,7 +30,7 @@ class Square:
 
     @property
     def position(self):
-        """Get/set the current position of the square"""
+        """Get/set the current position of the square."""
         return (self.__position)
 
     @position.setter
@@ -55,5 +55,5 @@ class Square:
         [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])]
-            [print(" ", end="") for k in range(0, self.__size)]
+            [print("#", end="") for k in range(0, self.__size)]
             print("")
