df19342c3327c35601369f77f1254e7065e0bdd0
thefarida <adamuibrahimfarida@gmail.com>
2022-10-12 10:42:54 +0100

    Define the size of a square based on 0-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Square Class"""
+
+
+class Square:
+
+    def __init__(self, size):
+        """__init__
+
+        The __init__ method intializes the size value
+        of the square.
+
+        Attributes:
+            size(int): The size of the square.
+
+        """
+        self.__size = size
