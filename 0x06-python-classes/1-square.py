daa4bb37f4318dd546d7d9c2db88a7ed6fa96152
thefarida <adamuibrahimfarida@gmail.com>
2022-10-12 11:12:25 +0100

    Update the size of the square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-"""Square Class"""
+"""Define a class Square."""
 
 
 class Square:
+    """Represent a square."""
 
     def __init__(self, size):
-        """__init__
+        """Initialize a new square.
 
-        The __init__ method intializes the size value
-        of the square.
-
-        Attributes:
-            size(int): The size of the square.
-
+        Args:
+            size (int): The size of the square.
         """
         self.__size = size
