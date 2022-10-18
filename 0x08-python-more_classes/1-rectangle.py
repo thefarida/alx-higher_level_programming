4e822507c67e7cca795d3ce9e89e3a6a484f828a
thefarida <adamuibrahimfarida@gmail.com>
2022-10-18 09:50:47 +0100

    Updated width and height

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -31,7 +31,7 @@ class Rectangle:
     @property
     def height(self):
         """Get/set the height of the rectangle."""
-        return self.__width
+        return self.__height
 
     @height.setter
     def height(self, value):
