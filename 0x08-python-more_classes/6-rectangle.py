e86ca3cef895e6148930ff5a93a42ec1787d1cfc
thefarida <adamuibrahimfarida@gmail.com>
2022-10-18 10:27:46 +0100

    Instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -81,4 +81,5 @@ class Rectangle:
 
     def __del__(self):
         """Print a message for every deletion of a rectangle."""
+        type(self).number_of_instances -= 1
         print("Bye rectangle...")
