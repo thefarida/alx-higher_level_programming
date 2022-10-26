2b5f3b447c36559ae874d98eea3d54c6582b1797
thefarida <adamuibrahimfarida@gmail.com>
2022-10-26 20:53:02 +0100

    Pascal Triangles

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -5,8 +5,7 @@
 def pascal_triangle(n):
     """Represent Pascal Triangle of size n.
 
-    Returns: 
-        A list of lists of integers representing the triangle.
+    Return a list of lists of integers representing the triangle.
     """
     if n <= 0:
         return []
@@ -15,7 +14,7 @@ def pascal_triangle(n):
     while len(triangles) != n:
         tri = triangles[-1]
         tmp = [1]
-        for i in rangle len(tri) - 1):
+        for i in range(len(tri) - 1):
             tmp.append(tri[i] + tri[i + 1])
         tmp.append(1)
         triangles.append(tmp)
