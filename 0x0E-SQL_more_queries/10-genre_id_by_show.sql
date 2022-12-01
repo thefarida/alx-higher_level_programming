7b9e2ccdebaf76a8aeb820fa02aeec53fe54e598
thefarida <adamuibrahimfarida@gmail.com>
2022-12-01 14:50:21 +0000

    SQL tasks and fix

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- list all shows in hbtn_0d_tvshows that have at least one genre linked.
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+        INNER JOIN `tv_show_genres` AS g
+	ON s.`id` = g.`show_id`
+ ORDER BY s.`title`, g.`genre_id`;
