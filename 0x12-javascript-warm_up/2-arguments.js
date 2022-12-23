8a24a47a8c7f63c33443aa024fd9a78ca98a27d1
thefarida <adamuibrahimfarida@gmail.com>
2022-12-23 13:39:49 +0000

    task

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 if (process.argv.length > 3) {
- console.log('Arguments found');
+  console.log('Arguments found');
 } else if (process.argv.length === 3){
- console.log('Argument found');
+  console.log('Argument found');
 } else {
- console.log('No argument');
+  console.log('No argument');
 }
