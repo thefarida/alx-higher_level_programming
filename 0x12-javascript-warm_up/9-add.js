a37db07361cdde5e23ae518a7a315dbe60d27f57
thefarida <adamuibrahimfarida@gmail.com>
2022-12-26 21:53:35 +0000

    Task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 function add (a, b) {
-    return a + b;
-  }
-  
-  console.log(add(Number(process.argv[2]), Number(process.argv[3])));
+  return a + b;
+}
+
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));
+
